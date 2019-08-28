8c0852e6e0651431eb48cf07102045772e3fc59c
Nazmus Shakib <nshakib.se@gmail.com>
2019-08-28 17:00:21 +0600

    Need to migrate company model

--- company/models.py
+++ company/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, null=True, blank=True)
+    company_email = models.EmailField()
+    company_phone = models.CharField(max_length=100, null=True, blank=True)
+    company_address = models.TextField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        ordering = ('company_name',)
